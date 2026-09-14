a0d1907627dbcce5c94036563dccc03ff2c731fd
pyj-0313 <pyjun0313@gmail.com>
2026-09-14 11:17:31 +0900

    chore: Spring Boot 프로젝트 초기 세팅

---
+++ src/main/java/com/linelock/linelock/LinelockApplication.java
@@ -0,0 +1,13 @@
+package com.linelock.linelock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinelockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinelockApplication.class, args);
+	}
+
+}
